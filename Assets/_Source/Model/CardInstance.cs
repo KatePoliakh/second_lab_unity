e6a191a5dbd4f1bcac121df5f01a649208b3dc5b
KatePoliakh <epolyakh.p@gmail.com>
2024-03-25 14:03:35 +0300

    feat: mvc architecture added

---
+++ Assets/_Source/Model/CardInstance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _Source.ScriptableObjects;
+using UnityEngine;
+
+namespace _Source.Model {
+    
+    public class CardInstance
+    {
+        public CardAsset cardAsset;
+        public int LayoutId;
+        public int CardPosition;
+
+        public CardInstance(CardAsset cardAsset)
+        {
+            this.cardAsset = cardAsset;
+        }
+    }
+
+    
+}
---
+++ Assets/_Source/View/CardView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _Source.Model;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Source.View {
+    
+    public class CardView : MonoBehaviour
+    {
+        [SerializeField]public Image cardImage;
+        [SerializeField]public Text cardName;
+        [SerializeField]public Color cardColor;
+        
+            private CardInstance _cardInstance;
+    
+
+            public void Init(CardInstance cardInstance)
+        {
+            _cardInstance = cardInstance;
+            cardName.text = cardInstance.cardAsset.cardName;
+            cardImage.sprite = cardInstance.cardAsset.cardImage;
+            cardColor = cardInstance.cardAsset.cardColor;
+            
+        }
+    }
+    
+}
