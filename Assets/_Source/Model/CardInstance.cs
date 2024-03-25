944a13c108a5bf6d5aa26051f8787f22b02b30b4
KatePoliakh <epolyakh.p@gmail.com>
2024-03-25 17:45:07 +0300

    feat: added graphics and card placement logic

--- Assets/_Source/Model/CardInstance.cs
+++ Assets/_Source/Model/CardInstance.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using _Source.ScriptableObjects;
+using _Source.View;
 using UnityEngine;
 
 namespace _Source.Model {
     
     public class CardInstance
     {
-        public CardAsset cardAsset;
-        public int LayoutId;
-        public int CardPosition;
+        public CardAsset СardAsset { get; private set; }
+        public CardView CardView { get; set; }
+        public int LayoutId { get; set; }
+        public int CardPosition { get; set; } 
+
 
         public CardInstance(CardAsset cardAsset)
         {
-            this.cardAsset = cardAsset;
+            this.СardAsset = cardAsset;
+        }
+        public void MoveToLayout(int layoutId)
+        {
+            LayoutId = layoutId;
+            // Update CardPosition based on the layout
         }
     }
 
--- Assets/_Source/View/CardView.cs
+++ Assets/_Source/View/CardView.cs
@@ -1,28 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using _Source.Core;
 using _Source.Model;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
 
 namespace _Source.View {
     
     public class CardView : MonoBehaviour
     {
         [SerializeField]public Image cardImage;
-        [SerializeField]public Text cardName;
+        [SerializeField] private TextMeshProUGUI cardName;
         [SerializeField]public Color cardColor;
+        [SerializeField] private GameObject cardBack;
+        [SerializeField] private GameObject cardFront;
         
             private CardInstance _cardInstance;
+            private bool _isFaceUp;
+            
+            public RectTransform RectTransform { get; private set; }
+            private void Awake()
+            {
+                RectTransform = transform as RectTransform;
+                if (RectTransform is null)
+                    Debug.LogError("CardView must be placed on gameobject with a RectTransform.");
+            }
+            public void OnPointerDown(PointerEventData eventData)
+            {
+                Debug.Log($"Clicked on card: {_cardInstance.СardAsset.cardName}");
+                PlayCard();
+            }
+            public void Flip(bool isFaceUp)
+            {
+                _isFaceUp = isFaceUp;
+                cardFront.SetActive(_isFaceUp);
+                cardBack.SetActive(!_isFaceUp);
+            }
+            private void PlayCard()
+            {
+                _cardInstance.MoveToLayout(CardGame.Instance.FieldLayoutId);
+            }
     
 
             public void Init(CardInstance cardInstance)
         {
             _cardInstance = cardInstance;
-            cardName.text = cardInstance.cardAsset.cardName;
-            cardImage.sprite = cardInstance.cardAsset.cardImage;
-            cardColor = cardInstance.cardAsset.cardColor;
+            
+            gameObject.name = _cardInstance.СardAsset.cardName;
+            cardName.text = _cardInstance.СardAsset.cardName;
+            
+            var sprite = _cardInstance.СardAsset.cardImage;
+            cardImage.sprite = sprite;
+            cardColor = cardInstance.СardAsset.cardColor;
             
         }
+            
     }
     
 }
